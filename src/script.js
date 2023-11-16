6e09a338a1f1fb193920e4c65f0790f35d4ef013
Samuel Thiken <samuel.thiken@grenoble-inp.org>
2023-11-16 10:20:03 +0100

    ajout fichiers de base

---
+++ src/script.js
@@ -0,0 +1,5 @@
+const DATASET_LINK = "https://github.com/owid/owid-datasets/blob/master/datasets/Environmental%20impacts%20of%20food%20(Clark%20et%20al.%202022)/Environmental%20impacts%20of%20food%20(Clark%20et%20al.%202022).csv";
+
+function getAndParseDataset() {
+  
+}
