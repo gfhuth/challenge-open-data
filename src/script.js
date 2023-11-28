7dd463796a87cf497d5ed6f11d2dd17161c84779
Guilherme Faccin Huth <guilherme.faccin-huth@grenoble-inp.org>
2023-11-28 09:45:36 +0100

    adding energy getters to Ingredient classe

--- src/script.js
+++ src/script.js
@@ -1,19 +1,30 @@
 const DATASET_LINK = "https://raw.githubusercontent.com/owid/owid-datasets/master/datasets/Environmental%20impacts%20of%20food%20(Clark%20et%20al.%202022)/Environmental%20impacts%20of%20food%20(Clark%20et%20al.%202022).csv";
 
-function getAndParseDataset() {
-  
-}
-
 
 class Ingredient {
-    constructor(_name, _ghg_kg, _gprot_kg, _gfat_kg, _gcarb_kg, _land_use_kg, _water_kg) {
-      this.name = _name;
+    constructor(_ghg_kg, _gprot_kg, _gfat_kg, _gcarb_kg, _land_use_kg, _water_kg) {
+    //   this.name = _name;
       this.ghg_kg = _ghg_kg;
       this.gprot_kg = _gprot_kg
       this.gfat_kg = _gfat_kg
       this.gcarb_kg = _gcarb_kg
       this.land_use_kg = _land_use_kg
       this.water_kg = _water_kg
+    }
+
+    get EnergyTotal_kg(){
+        return  this.EnergyProt_kg + 
+                this.EnergyFat_kg +
+                this.EnergyCarb_kg
+    }
+    get EnergyProt_kg(){
+        return this.gprot_kg*4
+    }
+    get EnergyFat_kg(){
+        return this.gprot_kg*9
+    }
+    get EnergyCarb_kg(){
+        return this.gcarb_kg*4
     }
 } 
 
@@ -27,12 +38,22 @@ class Recipe {
 async function parseCSV(){
     let data = await fetch(DATASET_LINK);
     data = await data.text();
-    data = await data.split('\n')
+    data = data.split('\n')
+    data = data.filter(Boolean)
     data.shift()
 
-    const objects = data.map(parse_rows2Ingredient)
-    return objects
+    data = data.map(parse_rows2Ingredient);
+    console.log(data)
+    data = data.reduce(objList2oneObj, {});
+    return data
 }
+
+function objList2oneObj(objetAccumulated, objetoCurrent){
+    objetAccumulated[objetoCurrent.name] = objetoCurrent.values;
+
+    return objetAccumulated;
+}
+
 
 function parse_rows2Ingredient(row){
         const values = row.split(',');
@@ -44,7 +65,6 @@ function parse_rows2Ingredient(row){
         const obj_land_use_kg = parseFloat(values[6])
         const obj_water_kg = parseFloat(values[18])
         const obj = new Ingredient(
-            obj_name,
             obj_ghg_kg,
             obj_gprot_kg,
             obj_gfat_kg,
@@ -52,7 +72,7 @@ function parse_rows2Ingredient(row){
             obj_land_use_kg,
             obj_water_kg
         );
-        return obj;
+        return {"name": obj_name, "values": obj};
 }
 
 ingredients = parseCSV()
