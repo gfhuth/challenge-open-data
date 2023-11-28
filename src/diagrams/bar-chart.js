f07ca50bd277195f2e61e9d8e915aa74585f71fe
Maud Bergonzoli <maud.bergonzoli@grenoble-inp.org>
2023-11-28 14:02:09 +0100

    pretty code

--- src/diagrams/bar-chart.js
+++ src/diagrams/bar-chart.js
@@ -19,7 +19,7 @@ var svg = d3.select("#bar-chart")
 // Create data
 //TODO : fonction pour appeler graphe avec les plats -> contient titre + ingrédients + valeur (pour chaque ingrédient) => formatb cf valeur de data
 function createBarChart(data) { 
-    // Creation of separated stack bars
+    // ---------- Creation of separated stack bars ----------
 	var datasets = [
         d3.stack()
             .keys(()=>{
@@ -27,7 +27,7 @@ function createBarChart(data) {
             Object.entries(data).forEach(([typeName,typeIngr]) => {
                 Object.keys(typeIngr['type1']).forEach(
                     (ingr, value) => {
-                        if (!ingredients.includes(ingr)) ingredients.push(ingr) //add to list
+                        if (!ingredients.includes(ingr)) ingredients.push(ingr) //add to list (no duplicata!!!)
                     } 
                     )
                 })
@@ -51,7 +51,8 @@ function createBarChart(data) {
             
             
     var num_groups = datasets.length;
-    
+
+    // ---------- Gestion des graphiques ----------
     var xlabels = data.map(function(d) {
         return d['Plate']
     });
@@ -74,28 +75,29 @@ function createBarChart(data) {
             return d[1];
         }));
     }));
-    
             
-    const y_1 = d3.scaleLinear() // first value
+    const y_1 = d3.scaleLinear() // value for type1 
     .domain([0, y1domain_max])
     .range([ height, 0 ]); 
     
-    const y_2 = d3.scaleLinear() // second value
+    const y_2 = d3.scaleLinear() // value for type2
     .domain([0, y2domain_max])
     .range([ height, 0 ]); 
     
-    var accent = d3.scaleOrdinal(d3.schemeBlues[6]); //! LES MEMES INGREDIENTS N'ONT PAS MEME COULEUR
     var xaxis = d3.axisBottom(x);
     var y1axis = d3.axisLeft(y_1);
     var y2axis = d3.axisRight(y_2);
     
+    var accent_green = d3.scaleOrdinal(d3.schemeGreens[6]); //! LES MEMES INGREDIENTS N'ONT PAS MEME COULEUR
+    var accent_red = d3.scaleOrdinal(d3.schemeReds[6]);
     
+    // ---------- Dessin des barres ----------
     d3.range(num_groups).forEach(function(gnum) {
         svg.selectAll('g.group' + gnum)
         .data(datasets[gnum])
         .enter()
         .append('g')
-        .attr('fill', accent)
+        .attr('fill', accent_green) //TODO modify color domain to split with two colors scheme (worth it?)
         .attr('class', 'group' + gnum)
         .selectAll('rect')
         .data(d => d)
@@ -109,6 +111,7 @@ function createBarChart(data) {
             if (gnum%2 == 0) {return y_1(d[0]) - y_1(d[1]);} else {return (y_2(d[0]) - y_2(d[1]));}}); 
     });
     
+    // ---------- Dessin des axes ----------
     svg.append('g')
     .attr('class', 'axis x')
     .attr('transform', 'translate(0,' + (height) + ")")
