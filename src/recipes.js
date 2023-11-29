ac2d857c9d085072aecdb422d5881260bb02684a
Anais Potel <anais.potel@grenoble-inp.org>
2023-11-29 15:34:04 +0100

    Croissant fixed

--- src/recipes.js
+++ src/recipes.js
@@ -47,15 +47,6 @@ export class Recipe {
         return qte_proteines;
     }
 
-    getPoidsTotal() {
-        poids = 0;
-        Object.keys(this.ingredients).forEach(key => {
-            let value = this.ingredients[key];
-            poids += value;
-        })
-        return poids;
-    }
-
     getApportCalorique() {
         const dico = {};
         Object.keys(this.ingredients).forEach(key => {
@@ -212,7 +203,7 @@ export function createRecipes() {
 
     /* Croissant */
     const croissant = new Recipe("croissant", "Croissant");
-    croissant.ingredients["croissant"] = 60;
+    croissant.ingredients["croissants"] = 60;
 
     /* Pain au chocolat */
     const pain_chocolat = new Recipe("pain_au_chocolat", "Pain au chocolat");
